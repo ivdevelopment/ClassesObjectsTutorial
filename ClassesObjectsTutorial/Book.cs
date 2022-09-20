2c9e7d61f9de94674beed59aebbb5a85cc0ae169
Ian Verlinden <Ian Verlinden@DESKTOP-TICN7DG>
2022-09-20 09:25:02 +0200

    Add project files.

---
+++ ClassesObjectsTutorial/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesObjectsTutorial
+{
+    internal class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        public Book()
+        {
+            title = "undefined";
+            author = "unknown";
+            pages = 0;
+        }
+        public Book(string Title, string Author, int Pages)
+        {
+            title = Title;
+            author = Author;
+            pages = Pages;
+        }
+        public void Afbeelden()
+        {
+            Console.WriteLine($"Titel: {title}");
+            Console.WriteLine($"Auteur: {author}");
+            Console.WriteLine($"Paginas: {pages}");
+            Console.WriteLine("--------------");
+        }
+
+    }
+}
---
+++ ClassesObjectsTutorial/Program.cs
@@ -0,0 +1,10 @@
+﻿using ClassesObjectsTutorial;
+
+Book book1 = new Book("Harry Potter", "J.K. Rowling", 400);
+book1.Afbeelden();
+
+Book book2 = new Book("Lord Of the Rings", "Tolkein", 700);
+book2.Afbeelden();
+
+Book book3 = new Book();
+book3.Afbeelden();
